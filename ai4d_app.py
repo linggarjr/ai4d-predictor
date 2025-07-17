9bcbf4add58980ebc9eeb2ac89c7e721fe372843
linggarjr <mantrimen335@gmail.com>
2025-07-17 16:29:52 +0700

    Update ai4d_app.py

--- ai4d_app.py
+++ ai4d_app.py
@@ -1,7 +1,8 @@
 import streamlit as st 
 import pandas as pd 
 import numpy as np 
-import joblib import os 
+import joblib 
+import os 
 import math from sklearn.linear_model 
 import LinearRegression from datetime 
 import datetime
